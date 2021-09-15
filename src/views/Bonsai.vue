089a5b0755b0549f8ea07e0cd4e5227a26d8ae0e
NassimBadaoui <nassim.badaoui@hotmail.fr>
2021-09-15 16:03:15 +0200

    axios = good

--- src/views/Bonsai.vue
+++ src/views/Bonsai.vue
@@ -21,29 +21,14 @@ export default {
             
        }
 
-    },  
+    }, 
+  created(){
+       this.axios.get('https://opentdb.com/api.php?amount=1&category=18')
+                  .then((response) => {
+                      console.log(response.data)
+                  })
+  } 
   
- methods: {
-                callServer: function () {
-                    console.log("tot");
-                    this.processingMessage = `Recherche en cours sur http://vlp042chr:3000/api/bonsai/?code_uf=${this.inputField}`;
-                    fetch(`http://localhost:8080/api/bonsai/`)
-                      .then(response => console.log(response.json()))
-                        /*.then(response => response.json())
-                        .then(response => {
-                            if ( Object.keys(response).length > 0 ) {
-                              console.log("Zooooo");
-                                this.serverResult = JSON.stringify(response)
-                            }
-                            else {
-                                this.serverResult = "Aucune structure correspondante trouvée"
-                            }
-                            this.processingMessage = ""
-                        })*/
-                        .catch(error => alert("Erreur : " + error ));
-                }
-                
-            }
 };
 </script>
 
